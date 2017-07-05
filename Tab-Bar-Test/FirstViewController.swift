2a117f6a8ffc2c26a1e9a28c7fec4904a30275fb
James Kang <jimmyjameskang@gmail.com>
2017-07-05 14:33:56 -0700

    Initial Commit

---
+++ Tab-Bar-Test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tab-Bar-Test
+//
+//  Created by James Kang on 7/5/17.
+//  Copyright © 2017 James Kang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tab-Bar-Test/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tab-Bar-Test
+//
+//  Created by James Kang on 7/5/17.
+//  Copyright © 2017 James Kang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
